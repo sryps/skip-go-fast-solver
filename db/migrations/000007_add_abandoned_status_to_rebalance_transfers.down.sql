5bf2f1873769d5c32fc9eba49eb0da78ffd0b1bd
nadim-az <160955402+nadim-az@users.noreply.github.com>
2024-11-26 11:23:25 -0500

    fix: add abandoned to possible statuses (#75)
    * fix: add abandoned to possible statuses
    
    * use new migration instead
    
    * wrap in begin/commit
    
    * remove begin/commit

---
+++ db/migrations/000007_add_abandoned_status_to_rebalance_transfers.down.sql
@@ -0,0 +1,19 @@
+ALTER TABLE rebalance_transfers RENAME TO rebalance_transfers_old;
+
+CREATE TABLE rebalance_transfers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    tx_hash TEXT NOT NULL,
+    source_chain_id      TEXT NOT NULL,
+    destination_chain_id TEXT NOT NULL,
+    amount TEXT NOT NULL,
+    status         TEXT NOT NULL DEFAULT 'PENDING',
+    CHECK (status IN ('PENDING', 'SUCCESS', 'FAILED'))
+);
+
+INSERT INTO rebalance_transfers 
+SELECT * FROM rebalance_transfers_old
+WHERE status != 'ABANDONED';
+
+DROP TABLE rebalance_transfers_old;
